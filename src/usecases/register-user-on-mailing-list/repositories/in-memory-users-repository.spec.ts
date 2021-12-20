83603831ecd03ae9e73983e27652e0eb3744945a
Klinton Lee <klintonlee92@gmail.com>
2021-12-20 03:36:18 -0300

    refactor: changed var name to sut (System Under Test)

--- src/usecases/register-user-on-mailing-list/repositories/in-memory-users-repository.spec.ts
+++ src/usecases/register-user-on-mailing-list/repositories/in-memory-users-repository.spec.ts
@@ -4,34 +4,34 @@ import { InMemoryUsersRepository } from './in-memory-users-repository'
 describe('in-memory-users-repository.spec.ts', () => {
   it('should return null if user it not found', async () => {
     const users: Array<IUserData> = []
-    const usersRepo = new InMemoryUsersRepository(users)
-    const user = await usersRepo.findByEmail('john.doe@email.com')
+    const sut = new InMemoryUsersRepository(users)
+    const user = await sut.findByEmail('john.doe@email.com')
     expect(user).toBeNull()
   })
 
   it('should return an user if found in the repository', async () => {
     const users: Array<IUserData> = []
-    const usersRepo = new InMemoryUsersRepository(users)
+    const sut = new InMemoryUsersRepository(users)
     const userMock = {
       name: 'John Doe',
       email: 'john.doe@email.com'
     }
-    await usersRepo.insertUser(userMock)
+    await sut.insertUser(userMock)
 
-    const user = await usersRepo.findByEmail('john.doe@email.com')
+    const user = await sut.findByEmail('john.doe@email.com')
     expect(user?.name).toBe('John Doe')
   })
 
   it('should return all users from repository', async () => {
     const users: Array<IUserData> = []
-    const usersRepo = new InMemoryUsersRepository(users)
+    const sut = new InMemoryUsersRepository(users)
     const usersMock = [
       { name: 'John Doe', email: 'john.doe@email.com' },
       { name: 'John Tre', email: 'john.tre@email.com' }
     ]
     users.push(...usersMock)
 
-    const allUsers = await usersRepo.findAllUsers()
+    const allUsers = await sut.findAllUsers()
     expect(allUsers).toEqual(usersMock)
   })
 })
