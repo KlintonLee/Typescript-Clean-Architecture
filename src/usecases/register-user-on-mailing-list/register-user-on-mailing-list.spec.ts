c938151be7d04dadcb3248928024716480b78243
Klinton Lee <klintonlee92@gmail.com>
2021-12-20 02:12:01 -0300

    test: should add user with complete data to mailing list

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,16 @@
+import { IUserData } from './dtos/user-data'
+
+describe('register-user-on-mailing-list.spec.ts', () => {
+  it('should add user with complete data on mailing list', async () => {
+    const users: Array<IUserData> = []
+    console.log(users)
+    // const repo: UsersRepository = new InMemoryUsersRepository(users)
+    // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+
+    // const name = 'John Doe'
+    // const email = 'john.doe@email.com'
+    // const response = await usecase.registerUserOnMailingList({ name, email })
+    // const user = await repo.findUserByEmail('john.doe@email.com')
+    // expect(user.name).toBe('John Doe')
+  })
+})
