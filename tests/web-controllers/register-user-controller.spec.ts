31d46cb9c84b2435718774815d0e70827b43a03a
Klinton Lee <klintonlee92@gmail.com>
2022-01-02 18:44:58 -0300

    refactor: register user controller test using beforeEach to avoid instance redundancy

--- tests/web-controllers/register-user-controller.spec.ts
+++ tests/web-controllers/register-user-controller.spec.ts
@@ -4,19 +4,27 @@ import { InMemoryUsersRepository } from '../usecases/repositories/in-memory-user
 import { RegisterUserOnMailingList } from '../../src/usecases/register-user-on-mailing-list'
 import { RegisterUserController } from '../../src/controllers/register-user-controller'
 
+let users: Array<IUserData>
+let repo: InMemoryUsersRepository
+let usecase: RegisterUserOnMailingList
+let controller: RegisterUserController
 describe('register-user-controller.spec.ts', () => {
-  it('should return status code 201 when requests contains valid user data', async() => {
+  beforeEach(() => {
+    users = []
+    repo = new InMemoryUsersRepository(users)
+    usecase = new RegisterUserOnMailingList(repo)
+
+    controller = new RegisterUserController(usecase)
+  })
+
+  it('should return status code 201 when requests contains valid user data', async () => {
     const request: IHttpRequest = {
       body: {
         name: 'John Doe',
         email: 'john.doe@email.com'
       }
     }
-    const users: Array<IUserData> = []
-    const repo: InMemoryUsersRepository = new InMemoryUsersRepository(users)
-    const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
 
-    const controller: RegisterUserController = new RegisterUserController(usecase)
     const response: IHttpResponse = await controller.handle(request)
     expect(response.statusCode).toBe(201)
     expect(response.body).toEqual(request.body)
