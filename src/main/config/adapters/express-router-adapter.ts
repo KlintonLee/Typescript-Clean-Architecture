603f6f672d13a2270ba05618fb2eafc9ae5a986a
Klinton Lee <klintonlee92@gmail.com>
2022-01-03 20:40:28 -0300

    feat: express route adapter

---
+++ src/main/config/adapters/express-router-adapter.ts
@@ -0,0 +1,16 @@
+import { RegisterUserController } from '../../../controllers'
+import { Request, Response } from 'express'
+import { IHttpRequest } from '../../../controllers/ports/http-request'
+
+const adaptRoute = (controller: RegisterUserController) => {
+  return async (req: Request, res: Response): Promise<Response> => {
+    const httpRequest:IHttpRequest = {
+      body: req.body
+    }
+
+    const httpResponse = await controller.handle(httpRequest)
+    return res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
+
+export { adaptRoute }
