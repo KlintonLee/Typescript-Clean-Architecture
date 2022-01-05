2da237d96d9d9237a114973f414ea89d54440bb0
Klinton Lee <klintonlee92@gmail.com>
2022-01-05 07:31:44 -0300

    test: config register route

---
+++ tests/main/config/routes/express-route.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest'
+import app from '../../../../src/main/config/app'
+
+describe('express-route.test.ts', () => {
+  it('should return an account on success', async () => {
+    app.post('/test_cors', (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .post('/api/register')
+      .send({
+        name: 'John Doe',
+        email: 'john.doe@email.com'
+      })
+      .expect(201)
+  })
+})
